ed4b73aa9d7bfdd7c7674da36868a2abc5033a7d
liqitian3344 <192253540@qq.com>
2018-11-01 15:17:02 +0800

    容器地址问题

--- src/main/java/com/bjwk/utils/qrcode/ZXingCodeUtil.java
+++ src/main/java/com/bjwk/utils/qrcode/ZXingCodeUtil.java
@@ -175,7 +175,7 @@ public class ZXingCodeUtil {
             ZXingConfig zxingconfig = new ZXingConfig();    // 实例化二维码配置参数
             zxingconfig.setHints(zp.getDecodeHintType());   // 设置二维码的格式参数
             zxingconfig.setContent(content);// 设置二维码生成内容
-            zxingconfig.setLogoPath("/webapp/WEB-INF/resources/logo.png"); // 设置Logo图片
+            zxingconfig.setLogoPath("/usr/local/tomcat/webapps/family-school/WEB-INF/resources/logo.png"); // 设置Logo图片
             //zxingconfig.setLogoPath("/root/family-school/qrcode_data/logo/logo1.png"); // 设置Logo图片
             zxingconfig.setLogoConfig(new LogoConfig());    // Logo图片参数设置
             zxingconfig.setLogoFlg(true);   // 设置生成Logo图片
