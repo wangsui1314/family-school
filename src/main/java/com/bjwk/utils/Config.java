391fe6b38699c6a47fc59dd4c62bf815434888e0
liqitian <liqitian3344@gmail.com>
2018-11-29 14:18:21 +0800

    123

---
+++ src/main/java/com/bjwk/utils/Config.java
@@ -0,0 +1,39 @@
+package com.bjwk.utils;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+/**
+ * @program: family-school
+ * @description: {类描述}
+ * @author: liqitian3344
+ * @create: 2018-11-29 14:11
+ */
+@Component
+@PropertySource(value = {"classpath:config.properties"})
+@Data
+public class Config {
+    @Value("${databaseurl}")
+    public String databaseurl;
+
+    @Value("${username}")
+    public String username;
+
+    @Value("${password}")
+    public String password;
+
+    @Value("${spring.elasticsearch.jest.uris}")
+    public String uris;
+
+    @Value("${redisHost}")
+    public String redisHost;
+
+    @Value("${redisPort}")
+    public String redisPort;
+
+    @Value("${redisPw}")
+    public String redisPw;
+
+}
--- src/main/java/com/bjwk/utils/RedisClient.java
+++ src/main/java/com/bjwk/utils/RedisClient.java
@@ -1,72 +1,82 @@
 package com.bjwk.utils;
 
 import org.aspectj.lang.annotation.SuppressAjWarnings;
+import org.springframework.beans.factory.annotation.Value;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
 public class RedisClient {
-	  private static JedisPool jedisPool = null;
-	    // Redis服务器IP
-	    private static String ADDR = "58.87.76.126";
-	    // Redis的端口号
-	    private static int PORT = 6379;
-	    // 访问密码
-	    private static String AUTH = "123";
+    private static JedisPool jedisPool = null;
+    /**
+     * Redis服务器IP
+     */
+    private static String ADDR = "58.87.76.126";
+    /**
+     * Redis的端口号
+     */
+    @Value("${redisPort}")
+    private static int PORT = 6379;
+    /**
+     * 访问密码
+     */
+    @Value("${redisPw}")
+    private static String AUTH = "123";
 
-	    /**
-	     * 初始化Redis连接池
-	     */
-	    static {
-	        try {
-	            JedisPoolConfig config = new JedisPoolConfig();
-	            // 连接耗尽时是否阻塞, false报异常,ture阻塞直到超时, 默认true
-	            config.setBlockWhenExhausted(true);
-	            // 设置的逐出策略类名, 默认DefaultEvictionPolicy(当连接超过最大空闲时间,或连接数超过最大空闲连接数)
-	            config.setEvictionPolicyClassName("org.apache.commons.pool2.impl.DefaultEvictionPolicy");
-	            // 是否启用pool的jmx管理功能, 默认true
-	            config.setJmxEnabled(true);
-	            // 最大空闲连接数, 默认8个 控制一个pool最多有多少个状态为idle(空闲的)的jedis实例。
-	            config.setMaxIdle(10);
-	            // 最大连接数, 默认8个
-	            config.setMaxTotal(400);
-	            // 表示当borrow(引入)一个jedis实例时，最大的等待时间，如果超过等待时间，则直接抛出JedisConnectionException；
-	            config.setMaxWaitMillis(1000 * 100);
-	            // 在borrow一个jedis实例时，是否提前进行validate操作；如果为true，则得到的jedis实例均是可用的；
-	            config.setTestOnBorrow(true);
-	            jedisPool = new JedisPool(config, ADDR, PORT, 3000, AUTH);
-	        } catch (Exception e) {
-	            e.printStackTrace();
-	        }
-	    }
-	    /**
-	     * 获取Jedis实例
-	     * 
-	     * @return
-	     */
-	    @SuppressAjWarnings("all")
-	    public synchronized static Jedis getJedis() {
-	        try {
-	            if (jedisPool != null) {
-	                Jedis resource = jedisPool.getResource();
-	                return resource;
-	            } else {
-	                return null;
-	            }
-	        } catch (Exception e) {
-	            e.printStackTrace();
-	        }
-	        return null;
-	    }
+    /**
+     * 初始化Redis连接池
+     */
+    static {
+        try {
+            JedisPoolConfig config = new JedisPoolConfig();
+            // 连接耗尽时是否阻塞, false报异常,ture阻塞直到超时, 默认true
+            config.setBlockWhenExhausted(true);
+            // 设置的逐出策略类名, 默认DefaultEvictionPolicy(当连接超过最大空闲时间,或连接数超过最大空闲连接数)
+            config.setEvictionPolicyClassName("org.apache.commons.pool2.impl.DefaultEvictionPolicy");
+            // 是否启用pool的jmx管理功能, 默认true
+            config.setJmxEnabled(true);
+            // 最大空闲连接数, 默认8个 控制一个pool最多有多少个状态为idle(空闲的)的jedis实例。
+            config.setMaxIdle(10);
+            // 最大连接数, 默认8个
+            config.setMaxTotal(400);
+            // 表示当borrow(引入)一个jedis实例时，最大的等待时间，如果超过等待时间，则直接抛出JedisConnectionException；
+            config.setMaxWaitMillis(1000 * 100);
+            // 在borrow一个jedis实例时，是否提前进行validate操作；如果为true，则得到的jedis实例均是可用的；
+            config.setTestOnBorrow(true);
+            jedisPool = new JedisPool(config, ADDR, PORT, 3000, AUTH);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	    /**
-	     * 释放jedis资源
-	     * 
-	     * @param jedis
-	     */
-	    public static void close(final Jedis jedis) {
-	        if (jedis != null) {
-	            jedis.close();
-	        }
-	    }
+    /**
+     * 获取Jedis实例
+     *
+     * @return
+     */
+    @SuppressAjWarnings("all")
+    public synchronized static Jedis getJedis() {
+        try {
+            if (jedisPool != null) {
+                Jedis resource = jedisPool.getResource();
+                return resource;
+            } else {
+                return null;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 释放jedis资源
+     *
+     * @param jedis
+     */
+    public static void close(final Jedis jedis) {
+        if (jedis != null) {
+            jedis.close();
+        }
+    }
 }
